5e5cdfb34aed01a28cd46ada1eb0a604e149cf4c
JongDeug <jongdeug2021@gmail.com>
2024-01-18 20:56:26 +0900

    Setting: NestJs DB initial setup
    NestJs 데이터베이스 초기 설정 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // TypeORM
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: parseInt(process.env['DB_PORT']),
+      username: process.env['DB_USERNAME'],
+      password: process.env['DB_PASSWORD'],
+      database: process.env['DB_DATABASE'],
+      entities: [],
+      // 개발할 때만 true, 실제 프로덕션은 false
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
