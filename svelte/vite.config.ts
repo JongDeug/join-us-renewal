1319b9a563561f9e66a15afaccfa50efe7bc68bc
JongDeug <jongdeug2021@gmail.com>
2024-01-18 20:48:39 +0900

    Setting: NestJs DB initial setup
    NestJs 데이터베이스 초기 설정 완료

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'vite';
 
 // scss global 옵션
 const scssOptions = {
-	preprocessorOptions: {
-		scss: {
-			additionalData: `@import '$lib/scss/app.scss';`
-		}
-	}
-}
+  preprocessorOptions: {
+    scss: {
+      additionalData: `@import '$lib/scss/app.scss';`,
+    },
+  },
+};
 export default defineConfig({
 	plugins: [sveltekit()],
 	// scss global 적용
